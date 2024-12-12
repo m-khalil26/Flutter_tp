15ba7b8146d39d912f4ec8c86a7695ca7c67aae7
Khali <khalilmakhloufi26@gmail.com>
2024-12-12 15:29:09 +0100

    cleaning

--- flutter_tp/lib/main.dart
+++ flutter_tp/lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (_) => PostsBloc(repository)..add(LoadPosts()),
-      child: MaterialApp(
+      child: const MaterialApp(
         home: PostsListScreen(),
       ),
     );
